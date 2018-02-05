be7e1505794a7f34cb586a51ec5f44fd2c786216
baby7305 <songhaibei7305@163.com>
2018-02-06 00:16:44 +0800

    JLabel测试排列图标

--- src/LabelEx.java
+++ src/LabelEx.java
@@ -10,45 +10,14 @@ public class LabelEx extends JFrame {
 
     private void initUI() {
 
-        String lyrics =  "<html>It's way too late to think of<br>" +
-                "Someone I would call now<br>" +
-                "And neon signs got tired<br>" +
-                "Red eye flights help the stars out<br>" +
-                "I'm safe in a corner<br>" +
-                "Just hours before me<br>" +
-                "<br>" +
-                "I'm waking with the roaches<br>" +
-                "The world has surrendered<br>" +
-                "I'm dating ancient ghosts<br>" +
-                "The ones I made friends with<br>" +
-                "The comfort of fireflies<br>" +
-                "Long gone before daylight<br>" +
-                "<br>" +
-                "And if I had one wishful field tonight<br>" +
-                "I'd ask for the sun to never rise<br>" +
-                "If God leant his voice for me to speak<br>" +
-                "I'd say go to bed, world<br>" +
-                "<br>" +
-                "I've always been too late<br>" +
-                "To see what's before me<br>" +
-                "And I know nothing sweeter than<br>" +
-                "Champaign from last New Years<br>" +
-                "Sweet music in my ears<br>" +
-                "And a night full of no fears<br>" +
-                "<br>" +
-                "But if I had one wishful field tonight<br>" +
-                "I'd ask for the sun to never rise<br>" +
-                "If God passed a mic to me to speak<br>" +
-                "I'd say stay in bed, world<br>" +
-                "Sleep in peace</html>";
+        JLabel lbl1 = new JLabel(new ImageIcon("res/cpu.png"));
+        JLabel lbl2 = new JLabel(new ImageIcon("res/drive.png"));
+        JLabel lbl3 = new JLabel(new ImageIcon("laptop.png"));
+        JLabel lbl4 = new JLabel(new ImageIcon("player.png"));
 
-        JLabel label = new JLabel(lyrics);
-        label.setFont(new Font("Serif", Font.PLAIN, 14));
-        label.setForeground(new Color(50, 50, 25));
+        createLayout(lbl1, lbl2, lbl3, lbl4);
 
-        createLayout(label);
-
-        setTitle("No Sleep");
+        setTitle("Icons");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -60,13 +29,20 @@ public class LabelEx extends JFrame {
         pane.setLayout(gl);
 
         gl.setAutoCreateContainerGaps(true);
+        gl.setAutoCreateGaps(true);
 
         gl.setHorizontalGroup(gl.createSequentialGroup()
                 .addComponent(arg[0])
+                .addComponent(arg[1])
+                .addComponent(arg[2])
+                .addComponent(arg[3])
         );
 
         gl.setVerticalGroup(gl.createParallelGroup()
                 .addComponent(arg[0])
+                .addComponent(arg[1])
+                .addComponent(arg[2])
+                .addComponent(arg[3])
         );
 
         pack();
