b20b891f544e281c97e3b4616bf0324ea85333f8
baby7305 <songhaibei7305@163.com>
2018-02-05 23:57:38 +0800

    JButton测试，可以配置图标文字

---
+++ src/ImageIconButtonEx.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageIconButtonEx extends JFrame {
+
+    public ImageIconButtonEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        ImageIcon saveIcon = new ImageIcon("res/save.png");
+        ImageIcon homeIcon = new ImageIcon("res/home.png");
+
+        JButton quitBtn = new JButton("Quit");
+        JButton saveBtn = new JButton(saveIcon);
+        JButton homeBtn = new JButton("Home", homeIcon);
+
+        createLayout(quitBtn, saveBtn, homeBtn);
+
+        setTitle("JButtons");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        gl.setAutoCreateGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+                .addComponent(arg[1])
+                .addComponent(arg[2])
+        );
+
+        gl.setVerticalGroup(gl.createParallelGroup()
+                .addComponent(arg[0])
+                .addComponent(arg[1])
+                .addComponent(arg[2])
+        );
+
+        gl.linkSize(arg[0], arg[1], arg[2]);
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            ImageIconButtonEx ex = new ImageIconButtonEx();
+            ex.setVisible(true);
+        });
+    }
+}
