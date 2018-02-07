37dacbd97f437eaf23220366fb5748a69dafe4bb
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-07 14:27:32 +0800

    水平不带刻度滑动条测试

--- src/SliderEx.java
+++ src/SliderEx.java
@@ -7,6 +7,11 @@ public class SliderEx extends JFrame {
 	private JSlider slider;
 	private JLabel lbl;
 
+	private ImageIcon mute;
+	private ImageIcon min;
+	private ImageIcon med;
+	private ImageIcon max;
+
 	public SliderEx() {
 
 		initUI();
@@ -14,24 +19,39 @@ public class SliderEx extends JFrame {
 
 	private void initUI() {
 
-		slider = new JSlider(0, 100, 0);
-		slider.setMinorTickSpacing(5);
-		slider.setMajorTickSpacing(10);
-		slider.setPaintTicks(true);
+		loadImages();
+
+		slider = new JSlider(0, 150, 0);
 
 		slider.addChangeListener((ChangeEvent event) -> {
-
 			int value = slider.getValue();
-			lbl.setText(Integer.toString(value));
+
+			if (value == 0) {
+				lbl.setIcon(mute);
+			} else if (value > 0 && value <= 30) {
+				lbl.setIcon(min);
+			} else if (value > 30 && value < 80) {
+				lbl.setIcon(med);
+			} else {
+				lbl.setIcon(max);
+			}
 		});
 
-		lbl = new JLabel("...");
+		lbl = new JLabel(mute, JLabel.CENTER);
 
 		createLayout(slider, lbl);
 
 		setTitle("JSlider");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+	}
+
+	private void loadImages() {
+
+		mute = new ImageIcon("mute.png");
+		min = new ImageIcon("res/min.png");
+		med = new ImageIcon("med.png");
+		max = new ImageIcon("res/max.png");
 	}
 
 	private void createLayout(JComponent... arg) {
@@ -43,12 +63,12 @@ public class SliderEx extends JFrame {
 		gl.setAutoCreateContainerGaps(true);
 		gl.setAutoCreateGaps(true);
 
-		gl.setHorizontalGroup(gl.createParallelGroup()
+		gl.setHorizontalGroup(gl.createSequentialGroup()
 				.addComponent(arg[0])
 				.addComponent(arg[1])
 		);
 
-		gl.setVerticalGroup(gl.createSequentialGroup()
+		gl.setVerticalGroup(gl.createParallelGroup()
 				.addComponent(arg[0])
 				.addComponent(arg[1])
 		);
