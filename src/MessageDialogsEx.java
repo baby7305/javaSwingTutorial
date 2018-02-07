1b52095b57f5dda9b2ed82162571b4b63fde4407
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-02-07 15:10:52 +0800

    规整代码

--- src/MessageDialogsEx.java
+++ src/MessageDialogsEx.java
@@ -7,110 +7,110 @@ import static javax.swing.GroupLayout.DEFAULT_SIZE;
 
 public class MessageDialogsEx extends JFrame {
 
-    private JPanel pnl;
+	private JPanel pnl;
 
-    public MessageDialogsEx() {
+	public MessageDialogsEx() {
 
-        initUI();
-    }
+		initUI();
+	}
 
-    private void initUI() {
+	private void initUI() {
 
-        pnl = (JPanel) getContentPane();
+		pnl = (JPanel) getContentPane();
 
-        JButton warBtn = new JButton("Warning");
-        JButton errBtn = new JButton("Error");
-        JButton queBtn = new JButton("Question");
-        JButton infBtn = new JButton("Information");
+		JButton warBtn = new JButton("Warning");
+		JButton errBtn = new JButton("Error");
+		JButton queBtn = new JButton("Question");
+		JButton infBtn = new JButton("Information");
 
-        warBtn.addActionListener(new ActionListener() {
+		warBtn.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                JOptionPane.showMessageDialog(pnl, "A deprecated call!",
-                        "Warning", JOptionPane.WARNING_MESSAGE);
-            }
-        });
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(pnl, "A deprecated call!",
+						"Warning", JOptionPane.WARNING_MESSAGE);
+			}
+		});
 
-        errBtn.addActionListener(new ActionListener() {
+		errBtn.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                JOptionPane.showMessageDialog(pnl, "Could not open file!",
-                        "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        });
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(pnl, "Could not open file!",
+						"Error", JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
-        queBtn.addActionListener(new ActionListener() {
+		queBtn.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                JOptionPane.showMessageDialog(pnl, "Are you sure to quit?",
-                        "Question", JOptionPane.QUESTION_MESSAGE);
-            }
-        });
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(pnl, "Are you sure to quit?",
+						"Question", JOptionPane.QUESTION_MESSAGE);
+			}
+		});
 
-        infBtn.addActionListener(new ActionListener() {
+		infBtn.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                JOptionPane.showMessageDialog(pnl, "Download completed.",
-                        "Information", JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(pnl, "Download completed.",
+						"Information", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 
-        createLayout(warBtn, errBtn , queBtn, infBtn);
+		createLayout(warBtn, errBtn, queBtn, infBtn);
 
-        setTitle("Message dialogs");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
+		setTitle("Message dialogs");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
-    private void createLayout(JComponent... arg) {
+	private void createLayout(JComponent... arg) {
 
-        Container pane = getContentPane();
-        GroupLayout gl = new GroupLayout(pane);
-        pane.setLayout(gl);
+		Container pane = getContentPane();
+		GroupLayout gl = new GroupLayout(pane);
+		pane.setLayout(gl);
 
-        gl.setAutoCreateGaps(true);
+		gl.setAutoCreateGaps(true);
 
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-                .addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(gl.createParallelGroup()
-                        .addComponent(arg[0])
-                        .addComponent(arg[2]))
-                .addGroup(gl.createParallelGroup()
-                        .addComponent(arg[1])
-                        .addComponent(arg[3]))
-                .addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+		gl.setHorizontalGroup(gl.createSequentialGroup()
+				.addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
+				.addGroup(gl.createParallelGroup()
+						.addComponent(arg[0])
+						.addComponent(arg[2]))
+				.addGroup(gl.createParallelGroup()
+						.addComponent(arg[1])
+						.addComponent(arg[3]))
+				.addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
+		);
 
-        gl.setVerticalGroup(gl.createSequentialGroup()
-                .addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(gl.createParallelGroup()
-                        .addComponent(arg[0])
-                        .addComponent(arg[1]))
-                .addGroup(gl.createParallelGroup()
-                        .addComponent(arg[2])
-                        .addComponent(arg[3]))
-                .addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+		gl.setVerticalGroup(gl.createSequentialGroup()
+				.addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
+				.addGroup(gl.createParallelGroup()
+						.addComponent(arg[0])
+						.addComponent(arg[1]))
+				.addGroup(gl.createParallelGroup()
+						.addComponent(arg[2])
+						.addComponent(arg[3]))
+				.addContainerGap(DEFAULT_SIZE, Short.MAX_VALUE)
+		);
 
-        gl.linkSize(arg[0], arg[1], arg[2], arg[3]);
+		gl.linkSize(arg[0], arg[1], arg[2], arg[3]);
 
-        pack();
-    }
+		pack();
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        EventQueue.invokeLater(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 
-            @Override
-            public void run() {
-                MessageDialogsEx md = new MessageDialogsEx();
-                md.setVisible(true);
-            }
-        });
-    }
+			@Override
+			public void run() {
+				MessageDialogsEx md = new MessageDialogsEx();
+				md.setVisible(true);
+			}
+		});
+	}
 }
